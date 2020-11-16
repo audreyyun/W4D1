dcbdf5de8b4af42fa97dddd36c1f73e72f807e04
Audrey Yun <96audreyyun@gmail.com>
2020-11-16 11:24:12 -0500

    refresh Knights, come up with game plan

--- knights_travails/knights.rb
+++ knights_travails/knights.rb
@@ -1,4 +1,5 @@
 require_relative 'knight_treenode.rb'
+require "set"
 
 class KnightPathFinder
     #move with confines of the board
@@ -10,31 +11,34 @@ class KnightPathFinder
         PolyTreeNode.new(start_pos)
     end
 
-    def self.valid_moves(pos)
+    def self.valid_moves(pos) #would give all 8 possible positions, regardless 
+        #pass in position, give all 8 moves
+        # visited.push(pos)
 
-
+        
     end
 
-    def initialize(start_pos)
-        @root = KnightPathFinder.root_node(start_pos)
-        @board = Array.new(8) {Array.new(8, [])}
-        @movetree = self.build_move_tree
+    def initialize(start_pos) #takes in an array
+        @root = KnightPathFinder.root_node(start_pos) #call in knight_treenode, create tree
+        # @board = Array.new(8) {Array.new(8, [])}
+        @movetree = self.build_move_tree(start_pos)
         @considered_positions = [start_pos]
+        # @considered_positions.push(start_pos) #come back to adding start position in initialize
     end
 
-    def build_move_tree(pos)
-        que = [pos]
+    def build_move_tree(pos) #bfs to find the next possible (8)positions
+        # que = [pos]
         
-        until que.empty?
-            el = que.shift
-            unless @considered_positions.include?(el)
-                self.class.valid_moves(el)
-        end
-        
+        # until que.empty?
+        #     el = que.shift
+        #     unless @considered_positions.include?(el)
+        #         self.class.valid_moves(el)
+        #     end
+        # end 
     end
 
     def new_move_positions(pos)
-        if !@considered_positions.include?(self.class.valid_moves(pos))
+        if !@considered_positions.include?(self.class.valid_moves(pos)) #will need to be rewritten to iterate through all valid moves 
             @considered_positions << pos
         end
         return @considered_positions
