dd4297c2a7b9a386a7b4de8f427d542ad172f8cf
Audrey Yun <96audreyyun@gmail.com>
2020-11-16 14:11:40 -0500

    finishing build move tree

--- knights_travails/knights.rb
+++ knights_travails/knights.rb
@@ -15,43 +15,41 @@ class KnightPathFinder
         possible_positions = [[1, 2],[1, -2],[2, 1],[2, -1],[-1, -2],[-1, 2],[-2, -1],[-2, 1]]
         
         all_moves = possible_positions.map do |position|
-            [pos[0] + position[0], pos[1] + position[1]]
+            [pos[0] + position[0], pos[1] + position[1]] 
         end
 
         all_moves.select do |position|
             (position[0] <= 7 && position[0] >= 0) && (position[1] <= 7 && position[1] >= 0)
         end
-        
+        #return an array of just positions
     end
 
-    def initialize(start_pos) #takes in an array
-        @root = KnightPathFinder.root_node(start_pos) #call in knight_treenode, create tree
-        # @board = Array.new(8) {Array.new(8, [])}
-        @movetree = self.build_move_tree(start_pos)
+    def initialize(start_pos) #takes in an array of start position
+        @root = KnightPathFinder.root_node(start_pos) #call in knight_treenode, create node
+        @movetree = self.build_move_tree
         @considered_positions = [start_pos]
-        # @considered_positions.push(start_pos) #come back to adding start position in initialize
     end
 
-    def build_move_tree(pos) #bfs to find the next possible (8)positions
-        # que = [pos]
+    def build_move_tree #To ensure that your tree represents only the shortest path to a given position, aka target
+        queue = [@root]
+        el = queue[0]
         
-        # until que.empty?
-        #     el = que.shift
-        #     unless @considered_positions.include?(el)
-        #         self.class.valid_moves(el)
-        #     end
-        # end 
+        until queue.empty?
+            el = queue.shift
+            queue += new_move_positions(el)
+        end 
     end
+
+
 
     def new_move_positions(pos)
         new_arr = []
-        self.class.valid_moves(pos).each do |position|
+        self.class.valid_moves(pos.value).each do |position|
             if !@considered_positions.include?(position)
-                new_arr << position
+                @considered_positions << position
+                new_arr << PolyTreeNode.new(position)
             end
         end
-
-        @considered_positions += new_arr
         new_arr
     end
 
