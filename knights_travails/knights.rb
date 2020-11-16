fd42b5b04c3f8dcfbf274ab60680b767d91bb054
David <davidjhinku@gmail.com>
2020-11-16 15:14:51 -0500

    Finish phase 2

--- knights_travails/knights.rb
+++ knights_travails/knights.rb
@@ -1,11 +1,11 @@
 require_relative 'knight_treenode.rb'
 require "set"
+require "byebug"
 
 class KnightPathFinder
     #move with confines of the board
     #can add/subtract a one/two or two/one per [row][col], and vise verse
     #we start at [0, 0] which is the root node
-    #
 
     def self.root_node(start_pos)
         PolyTreeNode.new(start_pos)
@@ -18,45 +18,51 @@ class KnightPathFinder
             [pos[0] + position[0], pos[1] + position[1]] 
         end
 
-        all_moves.select do |position|
+        final_moves = all_moves.select do |position|
             (position[0] <= 7 && position[0] >= 0) && (position[1] <= 7 && position[1] >= 0)
         end
         #return an array of just positions
+        final_moves
     end
 
     def initialize(start_pos) #takes in an array of start position
-        @root = KnightPathFinder.root_node(start_pos) #call in knight_treenode, create node
-        @movetree = self.build_move_tree
+        @root = KnightPathFinder.root_node(start_pos) #call in knight_treenode, create node. Will navigate from here
         @considered_positions = [start_pos]
+        self.build_move_tree
     end
 
-    def build_move_tree #To ensure that your tree represents only the shortest path to a given position, aka target
-        queue = [@root]
-        el = queue[0]
+    def build_move_tree #Should be accepting array of positions and converting to nodes, should add child to the parent 
+        queue = [@root] # make sure we're adding the nodes into the que
         
         until queue.empty?
             el = queue.shift
-            queue += new_move_positions(el)
+            positions_from_el = new_move_positions(el)
+            positions_from_el.each do |pos|
+                new_child = PolyTreeNode.new(pos)
+                new_child.parent = el
+                queue << new_child
+                el.children << new_child
+            end
         end 
     end
 
 
-
-    def new_move_positions(pos)
+    def new_move_positions(pos) #should just be returning arrays of positions, don't return notes
         new_arr = []
+
         self.class.valid_moves(pos.value).each do |position|
             if !@considered_positions.include?(position)
-                @considered_positions << position
-                new_arr << PolyTreeNode.new(position)
+                new_arr << position
             end
         end
+        @considered_positions += new_arr
         new_arr
     end
 
-    # def find_path(end_pos)
-    #     #build a move tree
-    # end
+    def find_path(end_pos)
+        #
 
 
+    end
 
 end
